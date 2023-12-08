cb45a42f023d8003c64dab6f003776989538caf6
Jackson C <jcalvert-lane@deloitte.com.au>
2023-12-08 12:51:55 +1100

    feat(tests): add testing files for logger

---
+++ tests/test_logger/test_logger.go
@@ -0,0 +1,11 @@
+package test_logger
+
+import (
+	"github.com/DeloitteOptimalReality/logxcept/pkg/logger"
+	"testing"
+)
+
+func TestLoggerInit(t testing.T) {
+	zl := logger.NewLogger()
+
+}
---
+++ tests/test_main.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"os"
+	"testing"
+)
+
+// Global setup and teardown
+func TestMain(m *testing.M) {
+
+	os.Exit()
+}
